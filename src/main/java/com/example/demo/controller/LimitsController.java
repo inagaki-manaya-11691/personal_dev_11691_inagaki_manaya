59e2235f7012d5b7d6672eb5a689b0adda64bad7
student <student@FJ-1025>
2026-05-21 16:53:58 +0900

    更新、削除、ユーザーIDごとの管理まで完成

--- src/main/java/com/example/demo/controller/LimitsController.java
+++ src/main/java/com/example/demo/controller/LimitsController.java
@@ -1,20 +1,5 @@
 package com.example.demo.controller;
 
-import java.security.Timestamp;
-import java.sql.Date;
-import java.util.List;
-
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-
-import com.example.demo.entity.Foods;
-import com.example.demo.repository.FoodsRepository;
-
-package com.example.demo.controller;
-
 import java.sql.Date;
 import java.time.LocalDateTime;
 import java.util.List;
@@ -22,24 +7,28 @@ import java.util.List;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import com.example.demo.entity.Categories;
-import com.example.demo.entity.Category;
-import com.example.demo.entity.Food;
+import com.example.demo.entity.Foods;
+import com.example.demo.entity.Users;
 import com.example.demo.repository.CategoriesRepository;
-import com.example.demo.repository.CategoryRepository;
-import com.example.demo.repository.FoodRepository;
+import com.example.demo.repository.FoodsRepository;
+import com.example.demo.repository.UsersRepository;
 
 @Controller
 public class LimitsController {
 	private final CategoriesRepository categoriesRepository;
 	private final FoodsRepository foodsRepository;
+	private final UsersRepository usersRepository;
 
-	public LimitsController(CategoriesRepository categoriesRepository, FoodsRepository foodsRepository) {
+	public LimitsController(CategoriesRepository categoriesRepository, FoodsRepository foodsRepository,
+			UsersRepository usersRepository) {
 		this.categoriesRepository = categoriesRepository;
 		this.foodsRepository = foodsRepository;
+		this.usersRepository = usersRepository;
 	}
 
 	//食品一覧表示
@@ -50,6 +39,9 @@ public class LimitsController {
 		// 全カテゴリー一覧を取得
 		List<Categories> categoryList = categoriesRepository.findAll();
 		model.addAttribute("categories", categoryList);
+
+		List<Users> userList = usersRepository.findAll();
+		model.addAttribute("users", userList);
 
 		// 食品一覧情報の取得
 		List<Foods> foodList = null;
@@ -70,22 +62,57 @@ public class LimitsController {
 		List<Categories> categoryList = categoriesRepository.findAll();
 		model.addAttribute("categories", categoryList);
 
-		return "addFood";
+		return "createlimits";
 	}
 
 	//新規登録処理
 	@PostMapping("/limits/add")
-	public String add(@RequestParam(defaultValue = "") Integer categoryId,
+	public String add(@RequestParam(defaultValue = "") Integer usersId,
+			@RequestParam(defaultValue = "") Integer categoryId,
 			@RequestParam(defaultValue = "") String foodname,
 			@RequestParam(defaultValue = "") Boolean limits,
 			@RequestParam(defaultValue = "") Date limitdate,
 			@RequestParam(defaultValue = "") String quantity) {
 
+		Integer quantity2 = Integer.parseInt(quantity);
 		Categories cate = categoriesRepository.findById(categoryId).get();
-		Foods food = new Foods(cate, foodname, limits, LocalDateTime.now(), limitdate, quantity);
+		Foods food = new Foods(usersId, cate, foodname, limits, LocalDateTime.now(), limitdate, quantity2);
 		foodsRepository.save(food);
 		return "redirect:/limits";
 	}
-}
 
+	@GetMapping("/limits/{foodId}/edit")
+	public String edit(@PathVariable Integer foodId, Model model) {
+		Foods foods = foodsRepository.findById(foodId).get();
+		model.addAttribute("foods", foods);
+		return "editlimits";
+	}
+
+	@PostMapping("/limits/{foodId}/edit")
+	public String update(@PathVariable Integer foodId,
+			@RequestParam(defaultValue = "") Integer categoryId,
+			@RequestParam(defaultValue = "") String foodname,
+			@RequestParam(defaultValue = "") Boolean limits,
+			@RequestParam(defaultValue = "") Date limitdate,
+			@RequestParam(defaultValue = "") String quantity) {
+
+		Foods foods = foodsRepository.findById(foodId).get();
+		Categories cate = categoriesRepository.findById(categoryId).get();
+		foods.setCategory(cate);
+		foods.setFoodname(foodname);
+		foods.setLimits(limits);
+		foods.setLimitdate(limitdate);
+		Integer quantity2 = Integer.parseInt(quantity);
+		foods.setQuantity(quantity2);
+
+		foodsRepository.save(foods);
+		return "redirect:/limits";
+	}
+
+	@PostMapping("/limits/{foodId}/delete")
+	public String delete(@PathVariable Integer foodId) {
+		foodsRepository.deleteById(foodId);
+
+		return "redirect:/limits";
+	}
 }
--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -12,3 +12,17 @@ INSERT INTO categories ( name) VALUES
 INSERT INTO users ( email, name, password) VALUES
 ( 'tanaka@aaa.com', '田中太郎', 'test123'),
 ( 'suzuki@aaa.com', '鈴木一郎', 'test456');
+
+--- foods テーブルにデータを挿入（田中太郎）
+INSERT INTO foods (category_id, users_id, foods_name, limits, createAt, limits_date, quantity) VALUES
+(1, 1, '米', false, '2026-05-20 15:00:00', '2026-05-25', 1),
+(2, 1, '肉', true, '2026-05-20 15:00:01', '2026-05-25', 1),
+(3, 1, '鮭', true, '2026-05-20 15:00:02', '2026-05-25', 1),
+(4, 1, 'りんご', true, '2026-05-20 15:00:03', '2026-05-25', 1),
+(5, 1, '水', false, '2026-05-20 15:00:04', '2026-05-25', 1),
+(6, 1, '醤油', true, '2026-05-20 15:00:05', '2026-05-25', 1),
+(7, 1, 'ドーナツ', true, '2026-05-20 15:00:06', '2026-05-25', 1);
+
+-- foods テーブルにデータを挿入（鈴木一郎）
+INSERT INTO foods (category_id, users_id, foods_name, limits, createAt, limits_date, quantity) VALUES
+(1, 2, '鈴木の米', false, '2026-05-20 15:00:00', '2026-05-25', 1);
